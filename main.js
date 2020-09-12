fc00e1752a57c6eace29dccc504adf4ce4f46113
Józef Żukowski <j.zukowski.edu@gmail.com>
2020-09-12 18:14:12 +0200

    adding java script

--- main.js
+++ main.js
@@ -2,3 +2,51 @@ const taskBox = document.querySelector('.mainDiv__contentDiv__readyTask');
 
 const addBtn = document.querySelector('#addBtn');
 const input = document.querySelector('.textInput');
+
+
+let i = 0;
+let task = [];
+let  readyTask = [];
+let title = [];
+let btnId = [];
+let checkbox = [];
+
+addBtn.addEventListener("click",function(){
+    task[i] = input.value;
+    if(task[i] == ""){
+        alert("You didn't enter anything");
+    }
+    else{
+        readyTask[i] = document.createElement("DIV");
+        taskBox.appendChild(readyTask[i]);
+        readyTask[i].classList.add("taskDiv");
+
+        title[i] = document.createElement("H1")                  
+        title[i].appendChild(document.createTextNode(task[i]));     
+        readyTask[i].appendChild(title[i]);
+
+        btnId[i] = document.createElement("BUTTON");
+        btnId[i].classList.add("delBtn");
+        btnId[i].value = i; 
+
+        btnId[i].addEventListener("click",function(){
+            readyTask[this.value].remove(); 
+        })
+  
+        checkbox[i] = document.createElement("BUTTON");
+        checkbox[i].classList.add("checkBox");
+        checkbox[i].value = i;
+        checkbox[i].addEventListener("click",function(){
+            checkbox[this.value].classList.toggle('check');
+        })
+
+        readyTask[i].appendChild(btnId[i]);        
+        readyTask[i].appendChild(checkbox[i]);
+
+        i ++;
+        input.value = "";   
+
+       
+
+    }
+})
